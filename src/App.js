a47ba0daf1515f9c7f836dfb46a2403780bc79e0
Elise Erickson <elisece717@gmail.com>
2024-01-21 08:53:49 -0500

    don't know if I understand or not

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>Hi I'm _______</h1>
+    <img alt="description" src="photos.google.com/dog"/>
+    <h2>About Me</h2>
+    <p>A paragraph all about me and my dog named Fern</p>
+    <a href="hi.com">Hi</a>
+    <a href="bye.com">bye</a>
+  </div>;
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,42 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    render(<App/>)
+    const element = screen.getByRole("heading", {
+        // name: /hi, i'm/i,
+        // exact: false,
+        level: 1
+    })
+    expect(element).toBeInTheDocument();
+})
+
+test("displays an image with alt text", () => {
+    render(<App/>)
+    const element = screen.getByRole("img")
+    expect(element).toHaveAttribute("src");
+})
+
+test("displays a second-level heading with the text About Me", () => {
+    render(<App/>)
+    const element = screen.getByRole("heading", {
+        name: "About Me",
+        level: 2
+    })
+    expect(element).toBeInTheDocument()
+})
+
+// test("displays a paragraph for your biography", () => {
+//     render(<App/>)
+//     const element = screen.getByRole("paragraph")
+//     expect(element).toBeInTheDocument()
+// })
+
+test("displays two links, one to your GitHub page, and one to your LinkedIn page", () => {
+    render(<App/>)
+    const elements = screen.getAllByRole("link")
+    expect(elements[0]).toHaveAttribute("href")
+    expect(elements[1]).toHaveAttribute("href")
+})
+
 // Your tests here
